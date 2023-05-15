dff5303d2de8db2fc7256ba12842275d318dece2
Kelvin Davis <273degreeskelvin@gmail.com>
2023-05-15 10:49:23 +1000

    Abstract function for selecting min segments in minCrossings

--- src/solvers/base_min_crossings_zigzag.rs
+++ src/solvers/base_min_crossings_zigzag.rs
@@ -1,11 +1,30 @@
+#[derive(Debug, Copy, Clone)]
 struct Segment<T> {
     start: usize,
     end: usize,
     data: T,
 }
 
-fn min_covering_segments<T>(n_loci: usize, semgents: Vec<Segment<T>>) -> Vec<Segment<T>> {
-    unimplemented!()
+fn min_covering_segments<T, I>(n_loci: usize, segments: I) -> Vec<Segment<T>> where
+    T: Copy,
+    I: IntoIterator<Item = Segment<T>>
+{
+    let mut segment_pigeonholes: Vec<Option<Segment<T>>> = (0..n_loci).map(|_| None).collect();
+    for c in segments {
+        let s = c.start;
+        let e = c.end;
+        if segment_pigeonholes[s].is_none() {
+            segment_pigeonholes[s] = Some(c);
+        } else {
+            segment_pigeonholes[s].as_mut().map(|c_old| {
+                if c_old.end < e {
+                    *c_old = c
+                } else {
+                }
+            });
+        }
+    }
+    segment_pigeonholes.iter().filter_map(|c| *c).collect()
 }
 
 fn min_cover_segments_from_groups<T>(

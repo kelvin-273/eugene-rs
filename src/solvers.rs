fac2325fa248c908da5f8b19c8ee97c1d1ece3e9
Kelvin Davis <kelvin.davis1@monash.edu>
2023-07-25 16:53:57 +1000

    File for lns solvers

--- src/solvers.rs
+++ src/solvers.rs
@@ -3,10 +3,6 @@
 //! Each solver has a function `breeding_program` which solves solves instances of various crossing
 //! schedule instances.
 
-/// Finds the min crossings for base problem using A*
-pub mod base_min_crossings_astar;
-/// Finds the min crossings for base problem using zigzag decomposition
-pub mod base_min_crossings_zigzag;
 /// Solves by breeding all possible progeny in each generation
 pub mod enumerator;
 /// Solves by breeding all non-dominating progeny in each generation
@@ -15,3 +11,9 @@ pub mod enumerator_dominance;
 pub mod greedy_base;
 /// Finds the min generations for multi-chromosome diploid plants
 pub mod greedy_multichrom_ti;
+/// Finds the min crossings for base problem using A*
+pub mod base_min_crossings_astar;
+/// Finds the min crossings for base problem using zigzag decomposition
+pub mod base_min_crossings_zigzag;
+/// Finds the min crossings for base problem using LNS approaches
+pub mod base_min_crossings_lns;

f8281d02519146ee7c08930cdca8400059082f0e
Kelvin Davis <kelvin.davis1@monash.edu>
2024-06-03 22:37:39 +1000

    Removed unused parts of base_min_crossings_backtracking and started on Distribute A*

--- src/solvers/base_min_crossings_backtracking.rs
+++ src/solvers/base_min_crossings_backtracking.rs
@@ -1,34 +1,9 @@
-use crate::plants::bit_array::*;
-
-pub fn min_crossings(n_loci: usize, pop_0: Vec<SingleChromGenotype>) -> Option<usize> {
-    if pop_0.contains(&SingleChromGenotype::ideotype(n_loci)) {
-        return Some(0);
-    }
-    unimplemented!();
-}
-
-pub fn min_crossings_distribtue(dist_array: &Vec<usize>) -> Option<usize> {
-    // TODO: assert valid dist_array <19-09-23> //
-    let n_loci = dist_array.len();
-    let _n_pop = dist_array.iter().max().unwrap() + 1;
-    let n_tree_cells = 2 * n_loci;
-    let mut _tree_left = vec![0; n_tree_cells];
-    let mut _tree_right = vec![0; n_tree_cells];
-    let mut tree_xs = vec![vec![vec![0; n_loci]; 2]; n_tree_cells];
-    // create each of the initial population
-    for (i, x) in dist_array.iter().enumerate() {
-        tree_xs[*x][0][i] = 1;
-        tree_xs[*x][1][i] = 1;
-    }
-    None
-}
-
 pub fn enumerate_covering_subsets<S>(_subsets: Vec<S>) {
     unimplemented!();
 }
 
 /// Given a distribute array, returns the ranges, as tuples (s, e), where for any range (s, e) and
-/// any i in [s, e] dist_array[i] cannot be found in dist_array outside of [s, e].
+/// any i in \[s, e\], dist_array\[i\] cannot be found in dist_array outside of \[s, e\].
 ///
 /// Example:
 /// ```
--- src/solvers/base_min_crossings_distribute_astar.rs
+++ src/solvers/base_min_crossings_distribute_astar.rs
@@ -1,6 +1,6 @@
+use crate::solution::BaseSolution;
+use itertools::Itertools;
 use pathfinding::prelude::*;
-
-pub struct BaseSolution {}
 
 pub fn breeding_program(dist_array: Vec<u8>) -> BaseSolution {
     let _res = astar(&dist_array, successors, heuristic, success);
@@ -11,8 +11,31 @@ fn heuristic(state: &Vec<u8>) -> u8 {
     get_n_pop(state) + get_n_loci(state)
 }
 
-fn successors(_state: &Vec<u8>) -> Vec<(Vec<u8>, u8)> {
-    // 
+fn successors(state: &Vec<u8>) -> Vec<(Vec<u8>, u8)> {
+    let n_pop = get_n_pop(state);
+    let mut out: Vec<(Vec<u8>, u8)> = vec![];
+    for gx in 0..n_pop - 1 {
+        for gy in gx + 1..n_pop {
+            // Note where the segment joins are and which direction they're going
+            let segjoins = state
+                .iter()
+                .tuple_windows()
+                .enumerate()
+                .filter(|(p, (&i, &j))| (i, j) == (gx, gy) || (i, j) == (gy, gx))
+                .map(|(p, (&i, &j))| match (i, j) == (gx, gy) {
+                    true => (p, CrossoverDirection::Forward),
+                    false => (p, CrossoverDirection::Backward),
+                })
+                .collect_vec();
+
+            if segjoins.is_empty() {
+                todo!();
+            } else {
+                todo!();
+            }
+            todo!();
+        }
+    }
     unimplemented!()
 }
 
@@ -26,4 +49,9 @@ fn get_n_pop(state: &Vec<u8>) -> u8 {
 
 fn get_n_loci(state: &Vec<u8>) -> u8 {
     state.len() as u8
+}
+
+enum CrossoverDirection {
+    Forward,
+    Backward,
 }

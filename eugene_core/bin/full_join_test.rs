0ceae9acdb6a55f2225804b117146c48e90b76ef
Kelvin Davis <kelvin.davis1@monash.edu>
2026-01-22 17:25:25 +1100

    btree_map_test, docs for DistArrays

--- eugene_core/bin/full_join_test.rs
+++ eugene_core/bin/full_join_test.rs
@@ -1,24 +1,257 @@
+use std::ops::Range;
+
+use eugene_core::plants::dist_array;
 use eugene_core::plants::dist_array::{DistArray, OrderedDistArray};
 use eugene_core::solvers::base_min_crossings_distribute_astar::{
     breeding_program_distribute_general, Config,
 };
-use eugene_core::{extra::instance_generators, plants::dist_array};
-use serde::{Deserialize, Serialize};
+use rand::seq::SliceRandom;
+use btree_range_map::RangeMap;
+use btree_range_map::Measure;
+use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct CompletedInstances {}
+/// A wrapper around DistArray to implement Ord and Measure traits.
+///
+//// This struct provides a way to compare DistArray instances and calculate distances
+/// between them based on their ordinal indices.
+///
+/// # Examples
+/// ```
+/// let da1 = DistArray::from(vec![0, 1, 0, 2, 1]);
+/// let da2 = DistArray::from(vec![0, 1, 2, 0]);
+/// let wrapper1 = DistArrayWrapper::new(da1);
+/// let wrapper2 = DistArrayWrapper::new(da2);
+///
+/// assert!(wrapper1 > wrapper2);
+/// assert_eq!(wrapper1.distance(&wrapper2), 5);
+/// ```
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize, Deserialize)]
+struct DistArrayWrapper(DistArray);
+
+impl DistArrayWrapper {
+    fn new(da: DistArray) -> Self {
+        DistArrayWrapper(da)
+    }
+
+    pub fn to_ordinal(&self) -> usize {
+        dist_array::ordinal_index(&self.0)
+    }
+}
+
+impl std::ops::Deref for DistArrayWrapper {
+    type Target = DistArray;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for DistArrayWrapper {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl PartialOrd for DistArrayWrapper {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        // Assumes that dist arrays are in canonical form
+        if self.0.len() != other.0.len() {
+            Some(self.0.len().cmp(&other.0.len()))
+        } else {
+            Some(self.0.cmp(&other.0))
+        }
+    }
+}
+
+impl Ord for DistArrayWrapper {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl Measure for DistArrayWrapper {
+    type Len = usize;
+
+    fn len(&self) -> usize {
+        1
+    }
+
+    fn distance(&self, other: &Self) -> usize {
+        if self.n_loci() > other.n_loci() || (self.n_loci() == other.n_loci() && self >= other) {
+            other.distance(self)
+        } else {
+            let ordinal_self = self.to_ordinal();
+            let ordinal_other = other.to_ordinal();
+            let mut dist = 0;
+            for n_loci in self.n_loci()..other.n_loci() {
+                dist += dist_array::count_distribute_instances(n_loci);
+            }
+            dist + (ordinal_other - ordinal_self)
+        }
+    }
+}
+
+fn f() -> Range<DistArrayWrapper> {
+    let start = DistArrayWrapper::new(DistArray::from(vec![0, 1, 0, 1]));
+    let end = DistArrayWrapper::new(DistArray::from(vec![0, 1, 2, 3]));
+    start..end
+}
+
+impl range_traits::Enum for DistArrayWrapper {
+    fn succ(&self) -> Option<Self> {
+        self.successor().map(DistArrayWrapper)
+    }
+
+    fn pred(&self) -> Option<Self> {
+        self.predecessor().map(DistArrayWrapper)
+    }
+}
+
+/// Represents the result of a completed instance comparison between HEU and OPT.
+///
+/// # Variants
+/// - `HeuEqualsOpt`: Indicates that the heuristic solution equals the optimal solution.
+/// - `HeuNotEqualsOpt`: Indicates that the heuristic solution does not equal the optimal
+/// solution.
+/// - `Timeout`: Indicates that the comparison resulted in a timeout.
+///
+/// # Examples
+/// ```
+/// let result = CompletedInstanceResult::HeuEqualsOpt;
+/// assert_eq!(result.to_bool(), Some(true));
+///
+/// let result = CompletedInstanceResult::HeuNotEqualsOpt;
+/// assert_eq!(result.to_bool(), Some(false));
+///
+/// let result = CompletedInstanceResult::Timeout;
+/// assert_eq!(result.to_bool(), None);
+///
+/// let result = CompletedInstanceResult::from_bool(true);
+/// assert_eq!(result, CompletedInstanceResult::HeuEqualsOpt);
+///
+/// let result = CompletedInstanceResult::from_bool(false);
+/// assert_eq!(result, CompletedInstanceResult::HeuNotEqualsOpt);
+/// ```
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Serialize, Deserialize)]
+enum CompletedInstanceResult {
+    HeuEqualsOpt,
+    HeuNotEqualsOpt,
+    Timeout,
+}
+
+impl CompletedInstanceResult {
+    /// Creates a CompletedInstanceResult from a `bool`.
+    ///
+    /// # Arguments
+    /// - `b`: A boolean indicating whether HEU equals OPT.
+    ///
+    /// # Returns
+    /// - `CompletedInstanceResult`: The corresponding CompletedInstanceResult.
+    ///
+    /// # Examples
+    /// ```
+    /// let result = CompletedInstanceResult::from_bool(true);
+    /// assert_eq!(result, CompletedInstanceResult::HeuEqualsOpt);
+    ///
+    /// let result = CompletedInstanceResult::from_bool(false);
+    /// assert_eq!(result, CompletedInstanceResult::HeuNotEqualsOpt);
+    /// ```
+    pub fn from_bool(b: bool) -> Self {
+        if b {
+            CompletedInstanceResult::HeuEqualsOpt
+        } else {
+            CompletedInstanceResult::HeuNotEqualsOpt
+        }
+    }
+
+    /// Converts the result to an `Option<bool>`.
+    ///
+    /// # Returns
+    /// - `Some(true)`: if HEU equals OPT.
+    /// - `Some(false)`: if HEU not equals OPT.
+    /// - `None`: if the result is a timeout.
+    ///
+    /// # Examples
+    /// ```
+    /// let result = CompletedInstanceResult::HeuEqualsOpt;
+    /// assert_eq!(result.to_bool(), Some(true));
+    ///
+    /// let result = CompletedInstanceResult::HeuNotEqualsOpt;
+    /// assert_eq!(result.to_bool(), Some(false));
+    ///
+    /// let result = CompletedInstanceResult::Timeout;
+    /// assert_eq!(result.to_bool(), None);
+    /// ```
+    pub fn to_bool(&self) -> Option<bool> {
+        match self {
+            CompletedInstanceResult::HeuEqualsOpt => Some(true),
+            CompletedInstanceResult::HeuNotEqualsOpt => Some(false),
+            CompletedInstanceResult::Timeout => None,
+        }
+    }
+
+    /// Returns true if the result indicates a timeout.
+    ///
+    /// # Returns
+    /// - `bool`: True if the result is a timeout, false otherwise.
+    ///
+    /// # Examples
+    /// ```
+    /// let result = CompletedInstanceResult::Timeout;
+    /// assert!(result.is_timeout());
+    /// ```
+    pub fn is_timeout(&self) -> bool {
+        matches!(self, CompletedInstanceResult::Timeout)
+    }
+
+    /// Returns true if the result indicates HEU equals OPT.
+    ///
+    /// # Returns
+    /// - `bool`: True if the result is HEU equals OPT, false otherwise.
+    ///
+    /// # Examples
+    /// ```
+    /// let result = CompletedInstanceResult::HeuEqualsOpt;
+    /// assert!(result.is_heu_equals_opt());
+    /// ```
+    pub fn is_heu_equals_opt(&self) -> bool {
+        matches!(self, CompletedInstanceResult::HeuEqualsOpt)
+    }
+
+    /// Returns true if the result indicates HEU not equals OPT.
+    ///
+    /// # Returns
+    /// - `bool`: True if the result is HEU not equals OPT, false otherwise.
+    ///
+    /// # Examples
+    /// ```
+    /// let result = CompletedInstanceResult::HeuNotEqualsOpt;
+    /// assert!(result.is_heu_not_equals_opt());
+    /// ```
+    pub fn is_heu_not_equals_opt(&self) -> bool {
+        matches!(self, CompletedInstanceResult::HeuNotEqualsOpt)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct CompletedInstances {
+    btree: RangeMap<DistArrayWrapper, CompletedInstanceResult>,
+}
 
 impl CompletedInstances {
     pub fn new() -> Self {
-        Self {}
+        CompletedInstances {
+            btree: RangeMap::new(),
+        }
     }
 
-    pub fn get(&self, _instance: &Vec<usize>) -> Option<bool> {
-        unimplemented!()
+    pub fn get(&self, da: &DistArrayWrapper) -> Option<CompletedInstanceResult> {
+        self.btree.get(da.clone()).copied()
     }
 
-    pub fn _insert(&mut self, _instance: &Vec<usize>, _value: bool) {
-        unimplemented!()
+    pub fn insert(&mut self, da: DistArrayWrapper, result: bool) {
+        let res_enum = CompletedInstanceResult::from_bool(result);
+        self.btree.insert(da, res_enum);
     }
 }
 
@@ -34,32 +267,48 @@ pub fn main() {
             print!("Solving instance: {:?}", xs);
 
             // Reverse check
-            let rev_xs: DistArray = dist_array::canonical_dist_array(&xs.iter().rev().collect());
-            if OrderedDistArray(xs.clone().into()) <= OrderedDistArray(rev_xs) {
+            let rev_xs: OrderedDistArray =
+                dist_array::canonical_dist_array(&xs.iter().rev().collect()).into();
+            if OrderedDistArray(xs.clone().into()) <= rev_xs {
                 print!(" Already completed in reverse. Skipping.\n");
                 continue;
             }
-            if let Some(completed) = db.get(&xs) {
+            if let Some(result) = db.get(&DistArrayWrapper(
+                OrderedDistArray(xs.clone().into()).into(),
+            )) {
                 print!(
                     " Already completed with result: {}. Skipping.\n",
-                    if completed { "HEU=OPT" } else { "HEU!=OPT" }
+                    match result {
+                        CompletedInstanceResult::HeuEqualsOpt => "HEU=OPT",
+                        CompletedInstanceResult::HeuNotEqualsOpt => "HEU!=OPT",
+                        CompletedInstanceResult::Timeout => "TIMEOUT",
+                    }
                 );
                 continue;
             }
 
-            let res_heu = breeding_program_distribute_general(&xs, &conf_heu);
-            print!(".");
-            if res_heu.is_none() {
+            let Some(res_heu) = breeding_program_distribute_general(&xs, &conf_heu) else {
                 println!(" HEU returned None");
                 break;
-            }
-            print!(".");
-            let res_opt = breeding_program_distribute_general(&xs, &conf_opt);
-            if res_opt.is_none() {
+            };
+            let Some(res_opt) = breeding_program_distribute_general(&xs, &conf_opt) else {
                 println!(" OPT returned None");
                 break;
+            };
+
+            if !(res_heu.objective() == res_opt.objective()) {
+                println!(" HEU!=OPT");
+            } else {
+                println!(" HEU=OPT");
             }
-            println!("");
+            let res = db.get(&DistArrayWrapper({
+                let rev = xs.iter().rev().cloned().collect::<Vec<usize>>();
+                dist_array::canonical_dist_array(&DistArray::from(rev))
+            }));
+            db.insert(
+                DistArrayWrapper(OrderedDistArray(xs.into()).into()),
+                res_heu.objective() == res_opt.objective()
+            );
         }
     }
 }
--- eugene_core/src/plants/dist_array.rs
+++ eugene_core/src/plants/dist_array.rs
@@ -1,12 +1,25 @@
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use std::iter::FromIterator;
 use std::ops::{Deref, DerefMut};
 
 // Description: A simple array-like structure that describes which gamete owns which locus.
 // Each locus is owned by exactly one gamete, and each gamete can own multiple loci.
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
 pub struct DistArray(Vec<usize>);
 
 impl DistArray {
     /// Returns the number of loci. Is equivalent to `self.len()`.
+    ///
+    /// # Returns
+    /// - `usize`: The number of loci in the DistArray.
+    ///
+    /// # Examples
+    /// ```
+    /// use dist_array::DistArray;
+    /// let xs = DistArray::from(vec![0, 1, 0, 1, 2]);
+    /// assert_eq!(xs.n_loci(), 5);
+    /// ```
     #[inline]
     pub fn n_loci(&self) -> usize {
         self.len()
@@ -17,6 +30,89 @@ impl DistArray {
         // assert that the values in data are in 0..n_pop
         assert!((0..self.n_loci()).all(|i| self[i] == 0 || self.contains(&(self[i] - 1))));
         self.0.iter().max().map_or(0, |&max| max + 1)
+    }
+
+    /// Checks if the current DistArray has `other` as its successor in lexicographic order.
+    ///
+    /// # Arguments
+    /// - `other`: The DistArray to check as a successor.
+    /// 
+    /// # Returns
+    /// - `bool`: True if `other` is the successor of the current DistArray, otherwise false.
+    ///
+    /// # Examples
+    /// ```
+    /// use dist_array::DistArray;
+    /// let xs = DistArray::from(vec![0, 1, 0, 1, 2]);
+    /// let succ = DistArray::from(vec![0, 1, 2, 0, 1]);
+    /// assert!(xs.has_successor(&succ));
+    /// ```
+    pub fn has_successor(&self, other: &Self) -> bool {
+        let n = self.len();
+        if other.len() < n || other.len() > n + 1 {
+            false
+        } else if other.len() == n + 1 {
+            *self == (0..n).collect::<DistArray>().into()
+                && *other == (0..n + 1).map(|x| x % 2).collect::<DistArray>().into()
+        } else {
+            let mut i = 0;
+            while i < n && self[i] == other[i] {
+                i += 1;
+            }
+            if i == n || other[i] != self[i] + 1 {
+                return false;
+            }
+            let mut a = 0;
+            i += 1;
+            while i < n && other[i] == a {
+                a = (a + 1) % 2;
+                i += 1;
+            }
+            return i == n;
+        }
+    }
+
+    /// Returns the successor DistArray in lexicographic order.
+    ///
+    /// # Returns
+    /// - `Option<DistArray>`: The successor DistArray if it exists, otherwise None.
+    ///
+    /// # Panics
+    /// - Panics if the DistArray is not in canonical form.
+    ///
+    /// # Examples
+    /// ```
+    /// use dist_array::DistArray;
+    /// let xs = DistArray::from(vec![0, 1, 0, 1, 2]);
+    /// let succ = xs.successor().unwrap();
+    /// assert_eq!(succ, DistArray::from(vec![0, 1, 2, 0, 1]));
+    /// ```
+    pub fn successor(&self) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        let n = self.len();
+        let mut xs = self.0.clone();
+
+        let mut xs_max = xs.clone();
+        for i in 1..n {
+            xs_max[i] = xs_max[i - 1].max(xs[i]);
+        }
+
+        for i in (0..n).rev() {
+            if i > 0 && xs[i] <= xs_max[i - 1] {
+                xs[i] += 1 + (xs[i] + 1 == xs[i - 1]) as usize;
+                for j in i + 1..n {
+                    xs[j] = if xs[j - 1] == 0 { 1 } else { 0 };
+                }
+                return Some(DistArray::from(xs));
+            } else if i > 0 && xs_max[i] == xs_max[i - 1] + 1 {
+                continue;
+            } else if i > 0 {
+                panic!("Either xs_max[i] < xs_max[i-1] - 1 or xs_max[i] > xs_max[i-1] + 1")
+            }
+        }
+        Some((0..n + 1).map(|x| x % 2).collect::<DistArray>().into())
     }
 }
 
@@ -166,6 +262,7 @@ pub fn canonical_dist_array(xs: &DistArray) -> DistArray {
     zs
 }
 
+#[derive(Clone, Debug)]
 pub struct OrderedDistArray(pub DistArray);
 
 impl PartialEq for OrderedDistArray {
@@ -178,7 +275,7 @@ impl Eq for OrderedDistArray {}
 
 impl std::hash::Hash for OrderedDistArray {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        for &x in &self.0.0 {
+        for &x in &self.0 .0 {
             x.hash(state);
         }
     }
@@ -259,10 +356,7 @@ pub fn first_full_join(xs: &DistArray) -> Option<(DistArray, usize, usize)> {
     None
 }
 
-pub fn random_redistribution(
-    xs: &DistArray,
-    rng: &mut impl rand::Rng,
-) -> (DistArray, usize, usize) {
+pub fn random_redistribution(xs: &DistArray, rng: &mut impl Rng) -> (DistArray, usize, usize) {
     let n_pop = xs.n_pop();
     assert!(n_pop >= 2);
     let (gx, gy) = loop {
@@ -322,6 +416,13 @@ impl DistArrayGenerator {
     }
 }
 
+impl Default for DistArrayGenerator {
+    fn default() -> Self {
+        Self::new(0)
+    }
+}
+
+
 impl Iterator for DistArrayGenerator {
     type Item = DistArray;
 
@@ -352,6 +453,32 @@ impl Iterator for DistArrayGenerator {
         None
     }
 }
+
+pub fn count_distribute_instances(n_loci: usize) -> usize {
+    if n_loci < 26 {
+        H_LOOKUP[n_loci - 1][0]
+    } else {
+        panic!("n_loci too large, will cause overflow");
+    }
+}
+
+pub fn ordinal_index(xs: &DistArray) -> usize {
+    let n = xs.n_loci();
+    let mut total = 1;
+    assert!(xs[0] == 0);
+    let mut x_max = 0;
+    for i in 1..n {
+        let x_next = x_max.max(xs[i]);
+        let m = xs[i] - (xs[i - 1] < xs[i]) as usize;
+        total += m * H_LOOKUP[n - i - 1][x_max];
+        x_max = x_next;
+    }
+    total
+}
+
+const H_LOOKUP: [[usize; 26]; 26] = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 0], [2, 5, 10, 17, 26, 37, 50, 65, 82, 101, 122, 145, 170, 197, 226, 257, 290, 325, 362, 401, 442, 485, 530, 577, 0, 0], [5, 15, 37, 77, 141, 235, 365, 537, 757, 1031, 1365, 1765, 2237, 2787, 3421, 4145, 4965, 5887, 6917, 8061, 9325, 10715, 12237, 0, 0, 0], [15, 52, 151, 372, 799, 1540, 2727, 4516, 7087, 10644, 15415, 21652, 29631, 39652, 52039, 67140, 85327, 106996, 132567, 162484, 197215, 237252, 0, 0, 0, 0], [52, 203, 674, 1915, 4736, 10427, 20878, 38699, 67340, 111211, 175802, 267803, 395224, 567515, 795686, 1092427, 1472228, 1951499, 2548690, 3284411, 4181552, 0, 0, 0, 0, 0], [203, 877, 3263, 10481, 29371, 73013, 163967, 338233, 649931, 1176701, 2025823, 3341057, 5310203, 8173381, 12232031, 17858633, 25507147, 35724173, 49160831, 66585361, 0, 0, 0, 0, 0, 0], [877, 4140, 17007, 60814, 190497, 529032, 1322035, 3017562, 6376149, 12616132, 23599287, 42061830, 71895817, 118485984, 189107067, 293386642, 443838525, 656471772, 951480319, 0, 0, 0, 0, 0, 0, 0], [4140, 21147, 94828, 372939, 1291020, 3967195, 10949772, 27499083, 63625324, 137144475, 278054700, 534575947, 981235788, 1729424859, 2940885580, 4844638155, 7757888172, 12111500443, 0, 0, 0, 0, 0, 0, 0, 0], [21147, 115975, 562595, 2409837, 9131275, 30785747, 93197715, 256118905, 646147067, 1512354975, 3315122947, 6861571205, 13504254315, 25423408747, 46017036275, 80427460497, 136237711195, 0, 0, 0, 0, 0, 0, 0, 0, 0], [115975, 678570, 3535027, 16360786, 67310847, 247126450, 815305195, 2438979402, 6681531511, 16926317722, 40012800675, 88981537570, 187474460527, 376521349986, 724665968347, 1342649618650, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [678570, 4213597, 23430840, 116393205, 516369838, 2050937445, 7330810572, 23754387325, 70378569810, 192349660173, 489109544320, 1166271373797, 2626214876310, 5619443518165, 11487973175508, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [4213597, 27644437, 163254885, 865549453, 4116416797, 17585497797, 67739250757, 236659281085, 755378218653, 2220256485877, 6057366816997, 15455199988077, 37134022033885, 84540738911653, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [27644437, 190899322, 1192059223, 6713065156, 34051164985, 155666739742, 643094785627, 2411993186248, 8263282235101, 26039675189890, 76028868158047, 207141221902732, 530149003318273, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [190899322, 1382958545, 9097183602, 54190360453, 291871399682, 1421428484337, 6270561900010, 25147234538837, 92145933070698, 310385944867057, 967429903483202, 2808702444248325, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1382958545, 10480142147, 72384727657, 454442481041, 2588914083065, 13377704321695, 62770605938897, 268176574842557, 1047553409432641, 3760903407286715, 12483001479080345, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [10480142147, 82864869804, 599211936355, 3952241526188, 23733360653955, 129659127547372, 644800210475939, 2924789433330540, 12141330682747843, 46331132144660780, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [82864869804, 682076806159, 5150665398898, 35590085232519, 224592570163192, 1293095848212799, 6793590696186174, 32614856716061623, 143461777606643524, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [682076806159, 5832742205057, 45891416030315, 331362825860749, 2191466128865567, 13259069937250169, 73376400893178667, 371765774619074885, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [5832742205057, 51724158235372, 423145657921379, 3185554606447814, 22024934452712437, 139671750579429512, 812024179978146887, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [51724158235372, 474869816156751, 4031845922290572, 31581598272055879, 227771488390279260, 1510382932875294447, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [474869816156751, 4506715738447323, 39645290116637023, 322516283206446897, 2421468886436411487, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [4506715738447323, 44152005855084346, 401806863439720943, 3389017736055752178, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [44152005855084346, 445958869294805289, 4192631462935194064, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [445958869294805289, 4638590332229999353, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [4638590332229999353, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
 #[cfg(test)]
 mod tests {
@@ -411,5 +538,15 @@ mod tests {
             canonical_dist_array(&DistArray(vec![5, 2, 6, 2, 5, 4])),
             DistArray(vec![0, 1, 2, 1, 0, 3])
         );
+    }
+
+    #[test]
+    fn ordinal_index_test() {
+        for n_loci in 1..11 {
+            for (i, xs) in DistArrayGenerator::new(n_loci).enumerate() {
+                let idx = ordinal_index(&xs);
+                assert_eq!(idx, i + 1, "Failed at xs = {:?}", xs);
+            }
+        }
     }
 }
